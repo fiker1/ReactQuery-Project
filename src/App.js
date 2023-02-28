38aa40b24eb0759a67e563cdad2d2858a36100fe
Birhan <58909711+fiker1@users.noreply.github.com>
2023-02-28 07:20:14 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Navbar from './componets/Navbar';
+import Planets from './componets/Planets';
+import People from './componets/People';
+import { ReactQueryDevtools } from 'react-query-devtools';
+
+
+function App() {
+  let [page, setPage] = useState('planets');
+
+  return (
+    <>
+      <div className="App">
+      <h1>LTech React Tutorial</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          { page === 'planets' ? <Planets /> : <People /> }
+        </div>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
+}
+
+export default App;
